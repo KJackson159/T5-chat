90a763a56a7a1b8d5d9a9e2c33780a90782b738c
KJackson159 <75142719+KJackson159@users.noreply.github.com>
2024-04-30 20:21:18 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB22MrbFxOErHtQ0NoYIBxAg7Sb6JC7eVY",
-    authDomain: "team5semesterproject.firebaseapp.com",
-    projectId: "team5semesterproject",
-    storageBucket: "team5semesterproject.appspot.com",
-    messagingSenderId: "75177646655",
-    appId: "1:75177646655:web:63626d40ee844e7972768a",
-    measurementId: "G-YJWPY6VGNT"
+    /*Insert your configuration here*/
 };
 
 // Initialize Firebase
